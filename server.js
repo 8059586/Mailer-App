2c748e49617ba7fa46df47eee3dec35c8bd4f673
8059586 <70647848+8059586@users.noreply.github.com>
2022-10-08 20:21:58 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+const { v4: uuidv4 } = require("uuid");
+
+var nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: '',
+        pass: '',
+    },
+    secure: true,
+});
+
+server.listen(process.env.PORT || 3030);
